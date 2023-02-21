89719480ee3b29eaa5de4bb912e4b50514ed418e
Nitjsefn <>
2023-02-21 20:17:28 +0100

    added handling commands for bot

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const {prefix, token} = require("./config.json");
 const { Client, GatewayIntentBits } = require("discord.js");
 const DCVoice = require("discord-voice");
-const bot = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates] });
+const bot = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.MessageContent] });
 const { log } = console;
 var BOT_NAME;
 
@@ -15,5 +15,17 @@ bot.on("messageUpdate", (old_msg, msg) => messageCreateAndUpdateMethod(msg));
 
 function messageCreateAndUpdateMethod(msg)
 {
-    log("Message");
+    if(msg.author.isBot) return;
+	if(msg.content[0] != prefix) return;
+	let arguments = msg.content.split(' ');
+	arguments[0] = arguments[0].slice(1);
+	let command = arguments.shift();
+	switch(command)
+	{
+		case "play": playCommand(msg, arguments); break;
+        case "pause": pauseCommand(msg); break;
+        case "stop": stopCommand(msg); break;
+		case "resume": resumeCommand(msg); break;
+        default: msg.reply("Wrong command!"); break;
+    }
 }
